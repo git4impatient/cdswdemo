11109c86020786b9447fd7cc3b5e8066e8eedee2
git4impatient <lurie@lurie.biz>
2017-05-02 11:05:42 -0400

    Update pairplot.py

--- pairplot.py
+++ pairplot.py
@@ -1,6 +1,10 @@
 # demonstrate how to pip install
 # from command line
-# this will fail without pip install seaborn
+# this will fail without
+# pip install seaborn
+# optional can use 
+# !pip install seaborn
+# in the workbench
 import seaborn as sns; sns.set(style="ticks", color_codes=True)
 iris = sns.load_dataset("iris")
 g = sns.pairplot(iris ,  hue="species" )
