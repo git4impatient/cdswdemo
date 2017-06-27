9506a8a2e01387b2bd496f7010104d9f330bda39
git4impatient <lurie@lurie.biz>
2017-06-27 14:24:58 -0400

    Create implyla.py

---
+++ implyla.py
@@ -0,0 +1,28 @@
+# connect to impala and run a simple query
+# plot result set
+# In command shell git clone https://github.com/cloudera/thrift_
+sasl
+# python setup.py build
+# python setup.py install
+#!pip install sasl
+#!pip install impyla
+#!pip install  thrift
+#!pip show thrift
+
+from impala.dbapi import connect
+conn = connect(host='10.142.0.2', port=21050)
+cursor = conn.cursor()
+cursor.execute('SELECT * FROM sample_07p LIMIT 10')
+print cursor.description  # prints the result set's schema
+results = cursor.fetchall()
+print results
+cursor.execute('select salary, total_emp from sample_07p where total_emp < 400000 limit 500')
+foo=300000*-0.0144608312694
+def plotit():
+ for row in cursor:
+    #print row[0] , row[1]
+    plt.scatter(row[1],row[0])
+ plt.plot([0, 300000], [50023, foo])
+ plt.show()
+plotit()
+print "done"
