12e26f390c1eeba411a4cce0df88960aa7278fbc
git4impatient <lurie@lurie.biz>
2017-06-27 17:57:50 -0400

    Update and rename impyla2.py to impylaOK.py

--- impylaOK.py
+++ impylaOK.py
@@ -15,4 +15,18 @@
 from impala.dbapi import connect
 conn = connect(host='10.142.0.2', port=21050)
 cursor = conn.cursor()
-cursor.execute('SELECT * FROM sample_07p LIMIT 10') 
+cursor.execute('SELECT * FROM sample_07p LIMIT 5') 
+
+print cursor.description  # prints the result set's schema
+results = cursor.fetchall()
+print results
+cursor.execute('select salary, total_emp from sample_07p where total_emp < 400000 limit 500')
+foo=300000*-0.0144608312694
+def plotit():
+ for row in cursor:
+    #print row[0] , row[1]
+    plt.scatter(row[1],row[0])
+ plt.plot([0, 300000], [50023, foo])
+ plt.show()
+plotit()
+print "done"
