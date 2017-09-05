70b3e71292f4ed77a4f2081acd0987ba88fd220b
git4impatient <lurie@lurie.biz>
2017-09-05 17:07:20 -0400

    Update 300addPackageLibrary.r

--- 300addPackageLibrary.r
+++ 300addPackageLibrary.r
@@ -7,7 +7,7 @@ library (datasets)
 library(MASS)
 ggplot(geyser) + geom_histogram(aes(x = duration))
 
-
+# these take a long time to install - please do not run on the demo cluster
 #install.packages("maps")
 #library(devtools)
 #devtools::install_github("rstudio/sparklyr")
