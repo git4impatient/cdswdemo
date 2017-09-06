d36379f11ff22381e9f247888e9970b91e366f63
git4impatient <lurie@lurie.biz>
2017-09-06 11:17:35 -0400

    Update 200_Basic_HelloR.r

--- 200_Basic_HelloR.r
+++ 200_Basic_HelloR.r
@@ -1,5 +1,10 @@
 thisIsAString <- "hello from R in cdsw"
 print ( thisIsAString )
 
+# do not install sparklyr on the demo cluster 
+# unless you have a specific
+# use case for it
+# it takes a long time to install
+#
 # to paralellize R 
 # devtools::install_github("rstudio/sparklyr")
