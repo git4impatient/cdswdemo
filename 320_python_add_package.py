3969bdb2ccd4f8503f5eba23755d322be0596883
git4impatient <lurie@lurie.biz>
2017-09-06 11:41:00 -0400

    Update 320_python_add_package.py

--- 320_python_add_package.py
+++ 320_python_add_package.py
@@ -5,6 +5,11 @@
 # optional can use 
 # !pip install seaborn
 # in the workbench
+#
+# once you install a package, it will be there the next time you work in the project
+# even though you stop the sessoin, a new session will still have the packages ready and waiting for you
+#
+
 import seaborn as sns; sns.set(style="ticks", color_codes=True)
 iris = sns.load_dataset("iris")
 g = sns.pairplot(iris ,  hue="species" )
