e79e70c0e7d76ed597e56f71d7182cb9b266831c
git4impatient <lurie@lurie.biz>
2019-01-29 16:04:37 -0500

    Update 200_Basic_HelloR.r

--- 200_Basic_HelloR.r
+++ 200_Basic_HelloR.r
@@ -1,6 +1,9 @@
 thisIsAString <- "hello from R in cdsw"
 print ( thisIsAString )
 
+x <- seq(-pi,pi,0.1)
+plot(x, sin(x))
+
 # do not install sparklyr on the demo cluster 
 # unless you have a specific
 # use case for it
