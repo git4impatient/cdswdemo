93034af19c251447adca6553161675a2df13e205
Marty <lurie@lurie.biz>
2019-08-20 08:29:43 -0400

    Update 300addPackageLibrary.r
    demonstrate failure to load package, install the package, it works
    
    THEN... fork the workspace and the A3 package is already installed and ready to go

--- 300addPackageLibrary.r
+++ 300addPackageLibrary.r
@@ -2,6 +2,15 @@
 # once you install a package, it will be there the next time you work in the project
 # even though you stop the sessoin, a new session will still have the packages ready and waiting for you
 #
+remove.packages("A3")
+a <- available.packages()
+#print (a)
+head(a,n=5L,3)
+
+# show failure to load package, go back, install A3, then it works
+# install.packages("A3")
+library ("A3")
+
 
 thisIsAString <- "Demonstrate how to install packages and use libraries in R"
 print ( thisIsAString )
