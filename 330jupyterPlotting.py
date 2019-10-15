4ba286b329c217012e9efa0db1fe0cd935116e02
Marty <lurie@lurie.biz>
2019-10-14 20:00:43 -0400

    Create 330jupyterPlotting.py

---
+++ 330jupyterPlotting.py
@@ -0,0 +1,29 @@
+# Import the necessary libraries
+# from https://www.datacamp.com/community/tutorials/seaborn-python-tutorial
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Initialize Figure and Axes object
+fig, ax = plt.subplots()
+
+# Load in data
+tips = pd.read_csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/tips.csv")
+
+# Create violinplot
+ax.violinplot(tips["total_bill"], vert=False)
+
+# Show the plot
+plt.show()
+
+# Import necessary libraries
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Load iris data
+iris = sns.load_dataset("iris")
+
+# Construct iris plot
+sns.swarmplot(x="species", y="petal_length", data=iris)
+
+# Show plot
+plt.show()
