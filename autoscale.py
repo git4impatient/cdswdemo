1e66484a3d0ce1fa5f09db43d4e6dd8d89c305fb
Marty <lurie@lurie.biz>
2022-09-27 15:56:40 -0400

    Create autoscale.py

---
+++ autoscale.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+import sys
+from random import random
+from operator import add
+from pyspark.sql import SparkSession
+
+# ask for large session to force autoscale
+
+spark = SparkSession\
+    .builder\
+    .appName("forceautoscale")\
+    .config("spark.authenticate", "true") \
+    .config ("spark.executor.instances", 12) \
+    .config("spark.executor.memory ", "18g") \
+    .config("spark.executor.cores", 4 ) \
+    .config("spark.scheduler.listenerbus.eventqueue.size  10000) \
+    .getOrCreate()
+sc = spark.sparkContext
